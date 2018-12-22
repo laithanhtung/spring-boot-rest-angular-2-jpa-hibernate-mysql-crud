783cfab3a05f7f80f1ea8896c072cab2e8eae0a1
Lai Thanh Tung <tungthanh.lai@gmail.com>
2018-12-22 18:28:19 +0700

    source code

---
+++ angular-demo/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { enableProdMode } from '@angular/core';
+
+import { AppComponent }  from './app.component';
+import { ArticleComponent }  from './article.component';
+import { ArticleService } from './article.service';
+
+@NgModule({
+  imports: [
+        BrowserModule,
+		HttpModule,
+		ReactiveFormsModule
+  ],
+  declarations: [
+        AppComponent,
+		ArticleComponent
+  ],
+  providers: [
+        ArticleService
+  ],
+  
+  bootstrap: [
+        AppComponent
+  ]
+})
+export class AppModule { }
